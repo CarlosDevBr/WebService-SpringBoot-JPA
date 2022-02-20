bc2c1df06a0c9c5ac6dc6b89b98ccadde65191a3
cahzerohenrique@gmail.com <CarlosDevBr>
2022-02-20 11:12:44 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/example/webservice/resources/ProductResource.java
+++ src/main/java/com/example/webservice/resources/ProductResource.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.webservice.entities.Product;
-import com.example.webservice.entities.Product;
 import com.example.webservice.services.ProductService;
 
 @RestController
