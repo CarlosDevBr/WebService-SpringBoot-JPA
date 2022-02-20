2b60b11b729a54ed6c22fa5457a6b3733ad40170
cahzerohenrique@gmail.com <CarlosDevBr>
2022-02-20 19:00:38 -0300

    Exception handling - findById

---
+++ src/main/java/com/example/webservice/resources/exceptions/StandardError.java
@@ -0,0 +1,72 @@
+package com.example.webservice.resources.exceptions;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public class StandardError implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+	private Instant timestamp;
+	private Integer status;
+	private String errors;
+	private String message;
+	private String path;
+	
+	public StandardError() {
+	}
+
+	public StandardError(Instant timestamp, Integer status, String errors, String message, String path) {
+		super();
+		this.timestamp = timestamp;
+		this.status = status;
+		this.errors = errors;
+		this.message = message;
+		this.path = path;
+	}
+
+	public Instant getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Instant timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getErrors() {
+		return errors;
+	}
+
+	public void setErrors(String errors) {
+		this.errors = errors;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+	
+	
+}
